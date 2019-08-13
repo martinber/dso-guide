58188b71ff3b501216c6140860cc61e485283593
Nano Remedi <agustoremedi@gmail.com>
2019-08-13 11:28:56 -0300

    simple flask script

---
+++ server/api.py
@@ -0,0 +1,27 @@
+import flask
+from flask import jsonify, request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+location = [
+    {'id': 0,
+     'nombre': 'martin',
+     'apellido': 'bernardi',
+     'ubicacion': 'ucacha'},
+    {'id': 1,
+     'nombre': 'augusto',
+     'apellido': 'remedi',
+     'ubicacion': 'rio cuarto'},
+    {'id': 2,
+     'nombre': 'ignacio',
+     'apellido': 'rittano',
+     'ubicacion': 'rio cuarto'},
+]
+
+@app.route('/api/v1/resources/all', methods = ['GET'])
+def get_all_locations():
+    return jsonify(location)
+
+
+app.run()
