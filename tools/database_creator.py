38be98457bee3811621f906628461dd2ba5c3b9a
Martin Bernardi (PC) <martinber.710@gmail.com>
2019-10-23 21:12:00 -0300

    database_creator style

--- tools/database_creator.py
+++ tools/database_creator.py
@@ -1,23 +1,68 @@
 import sqlite3
 from sqlite3 import Error
 
-
 db = sqlite3.connect('dso-guide.db')
 
 cur = db.cursor()
 
-cur.execute('CREATE TABLE users \
-(username text UNIQUE NOT NULL CHECK(typeof(username)=\"text\" and length(username) >=4 and length(username) <=32), \
-password text NOT NULL CHECK(typeof(password) == \"text\"), \
-lat float CHECK(typeof(lat) == \"real\" or typeof(lat) == "integer" and lat >= -90 and lat <=90), \
-lon float CHECK(typeof(lon) == \"real\" or typeof(lat) == "integer" and lon >= -180 and lon <=180), \
-salt text CHECK(typeof(salt) == \"text\"));')
+cur.execute('''
+    CREATE TABLE users
+    (
+        username text UNIQUE NOT NULL CHECK
+        (
+            TYPEOF(username) = "text"
+            AND LENGTH(username) >= 4 AND
+            LENGTH(username) <= 32
+        ),
+        password text NOT NULL CHECK
+        (
+            TYPEOF(password) == "text"
+        ),
+        lat float CHECK
+        (
+            TYPEOF(lat) == "real"
+            OR TYPEOF(lat) == "integer"
+            AND lat >= -90
+            AND lat <= 90
+        ),
+        lon float CHECK
+        (
+            TYPEOF(lon) == "real"
+            OR TYPEOF(lat) == "integer"
+            AND lon >= -180
+            AND lon <= 180
+        ),
+        salt text CHECK
+        (
+            TYPEOF(salt) == "text"
+        )
+    );
+    '''
+)
 
-cur.execute('CREATE TABLE watchlist \
-(star_id int NOT NULL CHECK(typeof(star_id) == \"integer\" and star_id >=0), \
-notes text CHECK(typeof(notes) == \"text\"),\
-style int CHECK(typeof(style) == \"integer\"), \
-username text NOT NULL CHECK(typeof(username) == \"text\"));')
+cur.execute('''
+    CREATE TABLE watchlist
+    (
+        star_id int NOT NULL CHECK
+        (
+            TYPEOF(star_id) == "integer"
+            AND star_id >= 0
+        ),
+        notes text CHECK
+        (
+            TYPEOF(notes) == "text"
+        ),
+        style int CHECK
+        (
+            TYPEOF(style) == "integer"
+        ),
+        username text NOT NULL CHECK
+        (
+            TYPEOF(username) == "text"
+        )
+    );
+    '''
+)
 
 cur.close()
 db.commit()
